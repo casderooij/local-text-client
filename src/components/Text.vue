57e14e5d02415492c24d5c2e271c239f7322e7a0
Cas de Rooij <casdrooij@gmail.com>
2018-04-18 16:12:43 +0200

    Build test

--- src/components/Text.vue
+++ src/components/Text.vue
@@ -1,11 +1,10 @@
 <template>
     <div class="text-container" v-if="text">
         <h2 class="text-title">{{ text.title }}</h2>
-        <h2>{{ message }}</h2>
 
         <div class="text-body">{{ text.body }}</div>
 
-        <div class="text-distance"></div>
+        <div class="text-distance" v-if="distance">{{ distance }}</div>
 
         <div class="text-button-container" v-if="isUser">
             <router-link class="text-link" :to="{name: 'TextUpdate', params: {id: id}}">
@@ -28,10 +27,62 @@ export default {
             id: '',
             text: null,
             isUser: false,
-            message: ''
+            latitude: '',
+            longitude: '',
+            distance: ''
         }
     },
     created() {
+        if('geolocation' in navigator) {
+
+            let position;
+
+            let success = (position) => {
+                this.counter++;
+                queryPosition(position.coords.latitude, position.coords.longitude);
+                function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+                    var R = 6371; // Radius of the earth in km
+                    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+                    var dLon = deg2rad(lon2-lon1); 
+                    var a = 
+                        Math.sin(dLat/2) * Math.sin(dLat/2) +
+                        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+                        Math.sin(dLon/2) * Math.sin(dLon/2)
+                        ; 
+                    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+                    var d = R * c; // Distance in km
+                    return d;
+                }
+
+                function deg2rad(deg) {
+                    return deg * (Math.PI/180)
+                }
+
+                this.distance = getDistanceFromLatLonInKm(position.coords.latitude, position.coords.longitude, this.text.latitude, this.text.longitude);
+            }
+
+            let error = (msg) => {
+                console.error(msg);
+            }
+
+            let queryPosition = (lat, lon) => {
+                this.latitude = lat;
+                this.longitude = lon;
+            }
+
+            navigator.geolocation.watchPosition(success, error, {
+                maximumAge: 0,
+                enableHighAccuracy: true,
+                timeout: Infinity
+            });
+
+        } else {
+            console.log('Geolocation not available...');
+        }
+
+
+
+
         this.id = window.location.href.split('/')[5];
 
         axios({
